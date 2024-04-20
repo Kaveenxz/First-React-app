57f6b7f6a369880555146f7dc301901fc39d91d1
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-20 12:03:16 +0530

    make new project

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,27 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Slider from './Slider'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='container'>
-      <h1>Hello I'm Kaveen</h1>
-
-      <div className="row">
-        <div className="col-6">
-          <Slider title="Bread" img={reactLogo}>Good bread</Slider>
-        </div>
-        <div className="col-6">
-          <Slider title="Rice" img={viteLogo}>Good rice</Slider>
-
-        </div>
-      </div>
-
-    </div>
+    <div></div>
   )
 }
 
