1ea256f26cd644a2e35ea8c24fa43a20f554fc54
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-20 13:17:49 +0530

    adding styles and finalized

--- demo-app/src/components/Info.tsx
+++ demo-app/src/components/Info.tsx
@@ -1,17 +1,21 @@
 import img from '../assets/profile.png'
 import '../App.css'
+import email from '../assets/Mail.png'
+import linkdin from '../assets/linkedin.png'
 
 function Info() {
   return (
-    <div>
+    <div className='info'>
         <img src={img} alt="logo" />
         <h2>Laura Smith</h2>
         <h4>Frontend Developer</h4>
         <h5>Laurasmith.website</h5>
 
-        <button>Email</button>
-        <button>Linkdin</button>
-
+        <div className="btns">
+        <button className='btn-mail'><img src={email} alt="" />Email</button> 
+        <span className='space'></span>
+        <button className='btn-linkdin'><img src={linkdin} alt="" />Linkdin</button>
+        </div>
     </div>
   )
 }
